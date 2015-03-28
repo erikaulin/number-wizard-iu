7c9155639765b748760a487e0f9da7f1585371ea
Erik Aulin <erik@aulin.co>
2015-03-28 08:41:17 +0100

    Revert "added build sceenes"
    This reverts commit 4adab8381897ab531c404b525ec095e6302152af.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,7 +5,6 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name){
 		Debug.Log("New Level load: "+ name);
-		Application.LoadLevel(name);
 	}
 
 	public void QuitRequest(){
