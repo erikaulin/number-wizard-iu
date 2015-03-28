f5d43956859f4a9cee46ca7cc0a93678fbad4a1b
Erik Aulin <erik@aulin.co>
2015-03-28 22:43:31 +0100

    Removed functions on LevelManager script

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,9 +5,19 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name){
 		Debug.Log("New Level load: "+ name);
+		Application.LoadLevel (name);
 	}
 
 	public void QuitRequest(){
 		Debug.Log("Quit requested");
+		Application.Quit();
+	}
+	
+	public void Higher(){
+		Debug.Log("Number is higher then: ");
+	}
+	
+	public void Lower(){
+		Debug.Log("Number is lower then: ");
 	}
 }
